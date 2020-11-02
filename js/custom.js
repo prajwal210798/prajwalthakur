f7391efa37086178f6532911274f35b5ac07ddac
prajwal210798 <prajwalthakur98@gmail.com>
2020-11-02 07:03:38 +0530

    eleven

--- js/custom.js
+++ js/custom.js
@@ -8,7 +8,7 @@
 		/*  PRELOADER ANIMATION
 		/* ----------------------------------------------------------- */
 	
-		var pageTransitionAnimationDuration = 200
+		var pageTransitionAnimationDuration = 500
 		var preloader = $(".preloader");
 		pageTransition({
 			target: document.querySelector('.page'),
